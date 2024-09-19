2a6c5a7c19f900e14f0475726fbb9f65ef7870fd
ROIHEE <thanawat58311@gmail.com>
2024-09-19 14:55:54 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://dsd.pcru.ac.th/production/sile/',
+        'url'		=> 'https://stock.fiservice.kmitl.ac.th/xampp/sile/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
